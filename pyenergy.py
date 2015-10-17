c4d4374a237294ae558842e334524db1df0f6a96
Matthew Daugherty <mattd@daughertyengr.com>
2015-10-17 17:47:36 -0500

    Scalable now

--- pyenergy.py
+++ pyenergy.py
@@ -11,10 +11,11 @@ class pyenergy:
         '''
         pass
 
-
-    def calc(self, Voltage, Amperage, axis=1):
+    def data_calc(self, Voltage, Amperage, axis=1):
         '''
             Works but only about 5% accurate
+            This will be extremely fast
+            if speed is a problem use this
         '''
         self.Vrms = np.sqrt(np.mean(np.square(Voltage), axis=axis))
         self.Arms = np.sqrt(np.mean(np.square(Amperage), axis=axis))
@@ -22,17 +23,31 @@ class pyenergy:
         self.W = np.mean((Voltage * Amperage), axis=axis)
         self.PF = self.W / self.VA
 
-    def calc2(self, time, Voltage, Amperage, axis=1):
+    def reg_calc(self, time, Voltage, Amperage, phase, axis=1):
         '''
-            The reccomended calcs to use
+            The recommended calcs to use
+            This only works with one phase
         '''
         fitfunc = lambda C, t: C[0]*np.sin(C[1]*t+C[2])
         errfunc = lambda params, t, Y: fitfunc(params, t) - Y
         init_p = np.array([100.0, 60.0, 0.0])
-        VC, success = leastsq(errfunc, init_p.copy(), args = (time, Voltage))
-        AC, success = leastsq(errfunc, init_p.copy(), args = (time, Amperage))
-        self.Vrms = VC[0]/sqrt(2)
-        self.Arms = AC[0]/sqrt(2)
-        self.VA = self.Vrms * self.Arms
-        self.PF = abs(cos(AC[2]-VC[2]))
-        self.W = self.VA * self.PF
+        VC, success = leastsq(errfunc, init_p.copy(), args=(time, Voltage))
+        AC, success = leastsq(errfunc, init_p.copy(), args=(time, Amperage))
+        self.Vrms[phase] = VC[0]/sqrt(2)
+        self.Arms[phase] = AC[0]/sqrt(2)
+        self.VA[phase] = self.Vrms[phase] * self.Arms[phase]
+        self.PF[phase] = abs(cos(AC[2]-VC[2]))
+        self.W[phase] = self.VA[phase] * self.PF[phase]
+
+    def calc(self, time, Voltage, Amperage, axis=1):
+        if Voltage.ndim > 1:
+            phase = len(Voltage)
+        else:
+            phase = 1
+        self.Vrms = np.zeros(phase)
+        self.Arms = np.zeros(phase)
+        self.VA = np.zeros(phase)
+        self.PF = np.zeros(phase)
+        self.W = np.zeros(phase)
+        for i in range(phase):
+            self.reg_calc(time, Voltage[i], Amperage[i], i)
--- test.py
+++ test.py
@@ -2,27 +2,31 @@
 import unittest
 from pyenergy import pyenergy
 import numpy as np
-from math import sin, sqrt, acos
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
+from math import sin, sqrt
 
-w = 60.0
-theta = 0.0
 samples = 200
 d = 3.5
-Vrms = 117.0
-Vpeak = Vrms*sqrt(2.0)
+w_all = np.array([60.0, 60.0, 60.0])
+theta_all = np.array([0.0, 0.0, 0.0])
+Vrms_all = np.array([117.0, 117.0, 117.0])
+Vpeak_all = Vrms_all*sqrt(2.0)
 time = np.array([t/samples for t in range(int(samples/d))])
-V = np.array([Vpeak*sin(w*float(t)/samples+theta) for t in range(int(samples/d))])
-PF = 0.85
-theta = acos(PF)
-Arms = 15.0
-Apeak = Arms*sqrt(2.0)
-A = np.array([Apeak*sin(w*float(t)/samples+theta) for t in range(int(samples/d))])
+V_list = []
+for w, theta, Vpeak in zip(w_all, theta_all, Vpeak_all):
+    V_list.append([Vpeak*sin(w*float(t)/samples+theta) for t in range(int(samples/d))])
+V = np.array(V_list)
+
+PF_all = np.array([1.0, 0.85, 0.65])
+theta_all = np.arccos(PF_all)
+Arms_all = np.array([15.0, 15.0, 15.0])
+Apeak_all = Arms_all*sqrt(2.0)
+A_list = []
+for w, theta, Apeak in zip(w_all, theta_all, Apeak_all):
+    A_list.append([Apeak*sin(w*float(t)/samples+theta) for t in range(int(samples/d))])
+A = np.array(A_list)
 
 p = pyenergy()
-p.calc2(time, V, A, axis=0)
+p.calc(time, V, A, axis=0)
 
 
 class TestPyEnergy(unittest.TestCase):
@@ -30,23 +34,24 @@ class TestPyEnergy(unittest.TestCase):
         pass
 
     def test_Vrms(self):
-        self.assertEqual(round(p.Vrms), round(Vrms))
+        for pVrms, Vrms in zip(p.Vrms, Vrms_all):
+            self.assertEqual(round(pVrms), round(Vrms))
 
     def test_Arms(self):
-        self.assertEqual(round(p.Arms), round(Arms))
+        for pArms, Arms in zip(p.Arms, Arms_all):
+            self.assertEqual(round(pArms), round(Arms))
 
     def test_VA(self):
-        self.assertEqual(round(p.VA), round(Vrms*Arms))
+        for pVA, VA in zip(p.VA, Vrms_all*Arms_all):
+            self.assertEqual(round(pVA), round(VA))
 
     def test_PF(self):
-        self.assertEqual(round(p.PF, 2), round(PF, 2))
+        for pPF, PF in zip(p.PF, PF_all):
+            self.assertEqual(round(pPF, 2), round(PF, 2))
 
     def test_W(self):
-        self.assertEqual(round(p.W), round(abs(Vrms*Arms*PF)))
+        for pW, W in zip(p.W, Vrms_all*Arms_all*PF_all):
+            self.assertEqual(round(pW), round(abs(W)))
 
 if __name__ == '__main__':
-        plt.plot(time, V/Vpeak)
-        plt.plot(time, A/Apeak)
-        plt.plot(time, (V/Vpeak * A/Apeak).clip(0))
-        plt.savefig('pye.png')
         unittest.main()
